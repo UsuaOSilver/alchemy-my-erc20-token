3f557f00d5dca8512cc074ebbd23bbf8a6a67b99
UsuaOSilver <anguyen8063@student.nocccd.edu>
2022-12-09 19:07:57 +0700

    write deploy script

--- contracts/KaToken.sol
+++ contracts/KaToken.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: Unlicense
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,16 @@
 async function main() {
+    const [deployer] = await ethers.getSigners();
     
+    console.log("Deploying contracts with the account: ", deployer.address);
+    
+    const weiAmount = (await deployer.getBalance()).toString();
+    
+    console.log("Account balance:", (await ethers.utils.formatEther(weiAmount)));
+    
+    const Token = await ethers.getcontractFactory("KaToken");
+    const token = await Token.deploy();
+    
+    console.log("Token address: ", token.address);
 }
 
 main ()
